41494d7cf9bcb21db916e1e2b83931351a264fd1
Evgeny Lukashevich <evgeny@positivedata.se>
2025-03-28 15:02:20 +0100

    sample code to reproduce

---
+++ Tree2SegmentLinesBug/Main.cs
@@ -0,0 +1,13 @@
+﻿
+using Spectre.Console;
+using Spectre.Console.Rendering;
+
+var textWriter = new StringWriter();
+var console = AnsiConsole.Create(new AnsiConsoleSettings());
+
+var tree = new Tree("Root");
+var child = tree.AddNode("Child 1");
+var child2 = child.AddNode("Child 2");
+
+var segments = tree.GetSegments(console);
+var lines = Segment.SplitLines(segments);
